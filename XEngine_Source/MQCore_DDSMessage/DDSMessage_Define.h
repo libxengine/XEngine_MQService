8d8ba4cedc9d4998fe982039962d728d9548ebb2
qyt <486179@qq.com>
2021-09-23 16:43:53 +0800

    added:dds module code

---
+++ XEngine_Source/MQCore_DDSMessage/DDSMessage_Define.h
@@ -0,0 +1,89 @@
+﻿#pragma once
+/********************************************************************
+//    Created:     2021/09/22  13:19:01
+//    File Name:   D:\XEngine_MQService\XEngine_Source\MQCore_DDSMessage\DDSMessage_Define.h
+//    File Path:   D:\XEngine_MQService\XEngine_Source\MQCore_DDSMessage
+//    File Base:   DDSMessage_Define
+//    File Ext:    h
+//    Project:     XEngine(网络通信引擎)
+//    Author:      qyt
+//    Purpose:     消息分发服务导出的函数
+//    History:
+*********************************************************************/
+//////////////////////////////////////////////////////////////////////////
+//                       导出的函数
+//////////////////////////////////////////////////////////////////////////
+extern "C" DWORD DDSMessage_GetLastError(int *pInt_SysError = NULL);
+/*************************************************************************
+                        订阅者与发布者公有函数
+**************************************************************************/
+/********************************************************************
+函数名称：DDSMessage_CommApi_Init
+函数功能：初始化DDS服务
+返回值
+  类型：逻辑型
+  意思：是否成功
+备注：
+*********************************************************************/
+extern "C" BOOL DDSMessage_CommApi_Init();
+/************************************************************************
+函数名称：DDSMessage_CommApi_Destory
+函数功能：销毁DDS服务
+返回值
+  类型：逻辑型
+  意思：是否销毁成功
+备注：
+************************************************************************/
+extern "C" BOOL DDSMessage_CommApi_Destory();
+/********************************************************************
+函数名称：DDSMessage_CommApi_TopicCreate
+函数功能：创建一个主题
+ 参数.一：pSt_DDSProtocol
+  In/Out：In/Out
+  类型：数据结构指针
+  可空：N
+  意思：输入主题信息,输出床边的信息
+返回值
+  类型：逻辑型
+  意思：是否成功
+备注：
+*********************************************************************/
+extern "C" BOOL DDSMessage_CommApi_TopicCreate(XENGINE_PROTOCOL_XDDS * pSt_DDSProtocol);
+/************************************************************************
+函数名称：DDSMessage_CommApi_TopicDelete
+函数功能：删除一个主题
+ 参数.一：pSt_DDSProtocol
+  In/Out：In
+  类型：数据结构指针
+  可空：N
+  意思：要删除的主题信息
+返回值
+  类型：逻辑型
+  意思：是否成功
+备注：
+************************************************************************/
+extern "C" BOOL DDSMessage_CommApi_TopicDelete(XENGINE_PROTOCOL_XDDS * pSt_DDSProtocol);
+/********************************************************************
+函数名称：DDSMessage_CommApi_TopicGet
+函数功能：获得主题关联信息
+ 参数.一：lpszTopicName
+  In/Out：In
+  类型：常量字符指针
+  可空：N
+  意思：输入要获取的主题名称
+ 参数.二：pppSt_DDSProtocol
+  In/Out：Out
+  类型：三级指针
+  可空：N
+  意思：输出主题关联列表,这个值需要释放内存
+ 参数.三：pInt_ListCount
+  In/Out：Out
+  类型：整数型指针
+  可空：N
+  意思：输出获取的个数
+返回值
+  类型：逻辑型
+  意思：是否成功
+备注：
+*********************************************************************/
+extern "C" BOOL DDSMessage_CommApi_TopicGet(LPCTSTR lpszTopicName, XENGINE_PROTOCOL_XDDS * **pppSt_DDSProtocol, int* pInt_ListCount);
---
+++ XEngine_Source/MQCore_DDSMessage/pch.cpp
@@ -0,0 +1,51 @@
+﻿#include "pch.h"
+#include "DDSMessage_CommApi/DDSMessage_CommApi.h"
+/********************************************************************
+//    Created:     2021/09/22  13:23:28
+//    File Name:   D:\XEngine_MQService\XEngine_Source\MQCore_DDSMessage\pch.cpp
+//    File Path:   D:\XEngine_MQService\XEngine_Source\MQCore_DDSMessage
+//    File Base:   pch
+//    File Ext:    cpp
+//    Project:     XEngine(网络通信引擎)
+//    Author:      qyt
+//    Purpose:     导出的函数实现
+//    History:
+*********************************************************************/
+BOOL XDDS_IsErrorOccur = FALSE;
+DWORD XDDS_dwErrorCode = 0;
+///////////////////////////////////////////////////////////////////////////////
+CDDSMessage_CommApi m_DDSCommApi;
+///////////////////////////////////////////////////////////////////////////////
+//                      导出函数实现
+///////////////////////////////////////////////////////////////////////////////
+extern "C" DWORD DDSMessage_GetLastError(int* pInt_SysError)
+{
+	if (NULL != pInt_SysError)
+	{
+		*pInt_SysError = errno;
+	}
+	return XDDS_dwErrorCode;
+}
+/*************************************************************************
+						   订阅者与发布者公有函数
+**************************************************************************/
+extern "C" BOOL DDSMessage_CommApi_Init()
+{
+	return m_DDSCommApi.DDSMessage_CommApi_Init();
+}
+extern "C" BOOL DDSMessage_CommApi_Destory()
+{
+	return m_DDSCommApi.DDSMessage_CommApi_Destory();
+}
+extern "C" BOOL DDSMessage_CommApi_TopicCreate(XENGINE_PROTOCOL_XDDS * pSt_DDSProtocol)
+{
+	return m_DDSCommApi.DDSMessage_CommApi_TopicCreate(pSt_DDSProtocol);
+}
+extern "C" BOOL DDSMessage_CommApi_TopicDelete(XENGINE_PROTOCOL_XDDS * pSt_DDSProtocol)
+{
+	return m_DDSCommApi.DDSMessage_CommApi_TopicDelete(pSt_DDSProtocol);
+}
+extern "C" BOOL DDSMessage_CommApi_TopicGet(LPCTSTR lpszTopicName, XENGINE_PROTOCOL_XDDS * **pppSt_DDSProtocol, int* pInt_ListCount)
+{
+	return m_DDSCommApi.DDSMessage_CommApi_TopicGet(lpszTopicName, pppSt_DDSProtocol, pInt_ListCount);
+}
