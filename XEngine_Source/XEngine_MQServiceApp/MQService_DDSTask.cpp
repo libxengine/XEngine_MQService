6741dca4224025d6ce2611d8b34ea1a9e82949d7
qyt <486179@qq.com>
2021-09-27 14:09:59 +0800

    dds support tcp protocol

--- XEngine_Source/XEngine_MQServiceApp/MQService_DDSTask.cpp
+++ XEngine_Source/XEngine_MQServiceApp/MQService_DDSTask.cpp
@@ -7,7 +7,6 @@ XHTHREAD CALLBACK MessageQueue_DDSMessage_ThreadDomain()
 	TCHAR tszMsgBuffer[2048];
 	TCHAR tszClientAddr[128];
 	XENGINE_PROTOCOLHDR st_ProtocolHdr;
-	XENGINE_PROTOCOL_XDDS st_DDSProtocol;
 
 	while (bIsRun)
 	{
@@ -15,7 +14,7 @@ XHTHREAD CALLBACK MessageQueue_DDSMessage_ThreadDomain()
 		memset(tszMsgBuffer, '\0', sizeof(tszMsgBuffer));
 		memset(tszClientAddr, '\0', sizeof(tszClientAddr));
 		memset(&st_ProtocolHdr, '\0', sizeof(XENGINE_PROTOCOLHDR));
-		memset(&st_DDSProtocol, '\0', sizeof(XENGINE_PROTOCOL_XDDS));
+		
 		//接收数据
 		if (!NetCore_BroadCast_Recv(hRVSocket, tszMsgBuffer, &nMsgLen, tszClientAddr))
 		{
@@ -38,36 +37,52 @@ XHTHREAD CALLBACK MessageQueue_DDSMessage_ThreadDomain()
 				XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_ERROR, _T("消息分发端:%s,无法处理的协议:%X,无法继续"), tszClientAddr, st_ProtocolHdr.unOperatorType);
 				continue;
 			}
-			if (XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_DDS_REQTOPICCREATE == st_ProtocolHdr.unOperatorCode)
-			{
-				memcpy(&st_DDSProtocol, tszMsgBuffer + sizeof(XENGINE_PROTOCOLHDR), sizeof(XENGINE_PROTOCOL_XDDS));
-				DDSMessage_CommApi_TopicCreate(&st_DDSProtocol);
-				ProtocolModule_Packet_DDSTopic(&st_DDSProtocol, tszMsgBuffer, &nMsgLen, XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_DDS_REPTOPICCREATE);
-				NetCore_BroadCast_Send(hSDSocket, tszMsgBuffer, nMsgLen);
-				XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("消息分发端:%s,请求创建主题成功,主题名:%s"), tszClientAddr, st_DDSProtocol.tszTopic);
-			}
-			else if (XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_DDS_REQTOPICDELETE == st_ProtocolHdr.unOperatorCode)
-			{
-				memcpy(&st_DDSProtocol, tszMsgBuffer + sizeof(XENGINE_PROTOCOLHDR), sizeof(XENGINE_PROTOCOL_XDDS));
-				DDSMessage_CommApi_TopicDelete(&st_DDSProtocol);
-				ProtocolModule_Packet_DDSTopic(&st_DDSProtocol, tszMsgBuffer, &nMsgLen, XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_DDS_REPTOPICDELETE);
-				NetCore_BroadCast_Send(hSDSocket, tszMsgBuffer, nMsgLen);
-				XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("消息分发端:%s,请求删除主题成功,主题名:%s"), tszClientAddr, st_DDSProtocol.tszTopic);
-			}
-			else if (XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_DDS_REQTOPICQUERY == st_ProtocolHdr.unOperatorCode)
-			{
-				int nListCount = 0;
-				XENGINE_PROTOCOL_XDDS** ppSt_DDSProtocol;
-				memcpy(&st_DDSProtocol, tszMsgBuffer + sizeof(XENGINE_PROTOCOLHDR), sizeof(XENGINE_PROTOCOL_XDDS));
-
-				DDSMessage_CommApi_TopicGet(st_DDSProtocol.tszTopic, &ppSt_DDSProtocol, &nListCount);
-				ProtocolModule_Packet_DDSQuery(&ppSt_DDSProtocol, nListCount, tszMsgBuffer, &nMsgLen);
-				NetCore_BroadCast_Send(hSDSocket, tszMsgBuffer, nMsgLen);
-				XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("消息分发端:%s,请求获取主题信息成功,主题名:%s"), tszClientAddr, st_DDSProtocol.tszTopic);
-				BaseLib_OperatorMemory_Free((XPPPMEM)&ppSt_DDSProtocol, nListCount);
-			}
+			MQService_DDSTask_Handle(&st_ProtocolHdr, tszClientAddr, tszMsgBuffer + sizeof(XENGINE_PROTOCOLHDR), nMsgLen - sizeof(XENGINE_PROTOCOLHDR));
 		}
 		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
 	return 0;
+}
+BOOL MQService_DDSTask_Handle(XENGINE_PROTOCOLHDR* pSt_ProtocolHdr, LPCTSTR lpszClientAddr, LPCTSTR lpszMsgBuffer, int nMsgLen)
+{
+	TCHAR tszMsgBuffer[2048];
+	XENGINE_PROTOCOL_XDDS st_DDSProtocol;
+
+	memset(tszMsgBuffer, '\0', sizeof(tszMsgBuffer));
+	memset(&st_DDSProtocol, '\0', sizeof(XENGINE_PROTOCOL_XDDS));
+
+	if (XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_DDS_REQTOPICCREATE == pSt_ProtocolHdr->unOperatorCode)
+	{
+		memcpy(&st_DDSProtocol, lpszMsgBuffer, sizeof(XENGINE_PROTOCOL_XDDS));
+		DDSMessage_CommApi_TopicCreate(&st_DDSProtocol);
+		ProtocolModule_Packet_DDSTopic(&st_DDSProtocol, tszMsgBuffer, &nMsgLen, XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_DDS_REPTOPICCREATE);
+		NetCore_BroadCast_Send(hSDSocket, tszMsgBuffer, nMsgLen);
+		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("消息分发端:%s,请求创建主题成功,主题名:%s"), lpszClientAddr, st_DDSProtocol.tszTopic);
+	}
+	else if (XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_DDS_REQTOPICDELETE == pSt_ProtocolHdr->unOperatorCode)
+	{
+		memcpy(&st_DDSProtocol, lpszMsgBuffer, sizeof(XENGINE_PROTOCOL_XDDS));
+		DDSMessage_CommApi_TopicDelete(&st_DDSProtocol);
+		ProtocolModule_Packet_DDSTopic(&st_DDSProtocol, tszMsgBuffer, &nMsgLen, XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_DDS_REPTOPICDELETE);
+		NetCore_BroadCast_Send(hSDSocket, tszMsgBuffer, nMsgLen);
+		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("消息分发端:%s,请求删除主题成功,主题名:%s"), lpszClientAddr, st_DDSProtocol.tszTopic);
+	}
+	else if (XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_DDS_REQTOPICQUERY == pSt_ProtocolHdr->unOperatorCode)
+	{
+		int nListCount = 0;
+		XENGINE_PROTOCOL_XDDS** ppSt_DDSProtocol;
+		memcpy(&st_DDSProtocol, lpszMsgBuffer, sizeof(XENGINE_PROTOCOL_XDDS));
+
+		DDSMessage_CommApi_TopicGet(st_DDSProtocol.tszTopic, &ppSt_DDSProtocol, &nListCount);
+		ProtocolModule_Packet_DDSQuery(&ppSt_DDSProtocol, nListCount, tszMsgBuffer, &nMsgLen);
+		NetCore_BroadCast_Send(hSDSocket, tszMsgBuffer, nMsgLen);
+		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("消息分发端:%s,请求获取主题信息成功,主题名:%s"), lpszClientAddr, st_DDSProtocol.tszTopic);
+		BaseLib_OperatorMemory_Free((XPPPMEM)&ppSt_DDSProtocol, nListCount);
+	}
+	else
+	{
+		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("消息分发端:%s,请求的协议无法处理,协议类型:%X"), lpszClientAddr, pSt_ProtocolHdr->unOperatorCode);
+	}
+
+	return TRUE;
 }
--- XEngine_Source/XEngine_MQServiceApp/MQService_DDSTask.h
+++ XEngine_Source/XEngine_MQServiceApp/MQService_DDSTask.h
@@ -1,3 +1,4 @@
 #pragma once
 
 XHTHREAD CALLBACK MessageQueue_DDSMessage_ThreadDomain();
+BOOL MQService_DDSTask_Handle(XENGINE_PROTOCOLHDR* pSt_ProtocolHdr, LPCTSTR lpszClientAddr, LPCTSTR lpszMsgBuffer, int nMsgLen);
