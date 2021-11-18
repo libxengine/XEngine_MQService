d9586252489bd839c8f2451524df031599a179db
qyt <486179@qq.com>
2021-11-18 10:11:03 +0800

    fixed:vscopy bat error update:types and configure added:userlist file

--- XEngine_Source/VSCopy_Debug.bat
+++ XEngine_Source/VSCopy_Debug.bat
@@ -11,3 +11,5 @@ copy /y "D:\XEngine\XEngine_SourceCode\Debug\HelpComponents_XLog.dll" "./"
 
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\RfcComponents_HttpServer.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\RfcComponents_WSProtocol.dll" "./"
+
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\NetHelp_APIHelp.dll" "./"
