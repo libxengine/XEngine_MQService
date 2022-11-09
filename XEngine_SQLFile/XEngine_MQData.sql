3c8fdf1bc9f40dbccb3da28020e07a42484e4344
qyt <486179@qq.com>
2022-11-09 09:28:11 +0800

    update:sql file

--- XEngine_SQLFile/XEngine_MQData.sql
+++ XEngine_SQLFile/XEngine_MQData.sql
@@ -3,15 +3,15 @@
 
  Source Server         : Test
  Source Server Type    : MySQL
- Source Server Version : 80030
+ Source Server Version : 80031 (8.0.31-0ubuntu0.20.04.1)
  Source Host           : 192.168.1.12:3306
  Source Schema         : XEngine_MQData
 
  Target Server Type    : MySQL
- Target Server Version : 80030
+ Target Server Version : 80031 (8.0.31-0ubuntu0.20.04.1)
  File Encoding         : 65001
 
- Date: 01/08/2022 13:15:31
+ Date: 04/11/2022 10:47:16
 */
 
 SET NAMES utf8mb4;
@@ -30,6 +30,7 @@ CREATE TABLE `XEngine_CommKey`  (
   `tszQueuePublishTime` varchar(128) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '发布时间',
   `tszQueueData` varchar(8192) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '保存数据',
   `nDataLen` int NOT NULL COMMENT '数据大小',
+  `nDataType` tinyint NULL DEFAULT NULL COMMENT '数据类型',
   `tszQueueCreateTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '插入时间',
   PRIMARY KEY (`ID`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = DYNAMIC;
