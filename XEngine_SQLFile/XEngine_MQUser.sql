52b8ee301f4388da56ca86d4c80475a98e168776
qyt <486179@qq.com>
2023-02-22 16:12:10 +0800

    update:sql file

--- XEngine_SQLFile/XEngine_MQUser.sql
+++ XEngine_SQLFile/XEngine_MQUser.sql
@@ -3,19 +3,30 @@
 
  Source Server         : Test
  Source Server Type    : MySQL
- Source Server Version : 80031 (8.0.31-0ubuntu0.20.04.2)
- Source Host           : 192.168.1.12:3306
+ Source Server Version : 80032 (8.0.32-0ubuntu0.20.04.2)
+ Source Host           : 192.168.1.10:3306
  Source Schema         : XEngine_MQUser
 
  Target Server Type    : MySQL
- Target Server Version : 80031 (8.0.31-0ubuntu0.20.04.2)
+ Target Server Version : 80032 (8.0.32-0ubuntu0.20.04.2)
  File Encoding         : 65001
 
- Date: 03/01/2023 16:39:05
+ Date: 22/02/2023 16:11:42
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for KeyOwner
+-- ----------------------------
+DROP TABLE IF EXISTS `KeyOwner`;
+CREATE TABLE `KeyOwner`  (
+  `tszUserName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '所属用户',
+  `tszKeyName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '拥有的队列名称',
+  `tszCreateTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`tszKeyName`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for UserInfo
@@ -33,7 +44,7 @@ CREATE TABLE `UserInfo`  (
   `tszLoginTime` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '登录时间',
   `tszCreateTime` datetime NOT NULL COMMENT '注册时间',
   PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for UserKey
@@ -47,7 +58,7 @@ CREATE TABLE `UserKey`  (
   `tszUPTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
   `tszCreateTime` datetime NOT NULL COMMENT '创建的时间',
   PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for UserTime
@@ -56,7 +67,8 @@ DROP TABLE IF EXISTS `UserTime`;
 CREATE TABLE `UserTime`  (
   `tszQueueName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '队列名称',
   `nIDMsg` bigint NOT NULL COMMENT '队列唯一ID',
-  `nIDTime` bigint NOT NULL COMMENT '发布时间'
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  `nIDTime` bigint NOT NULL COMMENT '发布时间',
+  `tszCreateTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;
