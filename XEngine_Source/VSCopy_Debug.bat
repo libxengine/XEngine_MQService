f85c7ab0b55a7925d08d73431f88edc52c21bc11
qyt <486179@qq.com>
2023-04-25 13:17:32 +0800

    update:makefile and vscopy debug

--- XEngine_Source/VSCopy_Debug.bat
+++ XEngine_Source/VSCopy_Debug.bat
@@ -13,3 +13,5 @@ copy /y "D:\XEngine\XEngine_SourceCode\Debug\HelpComponents_DataBase.dll" "./"
 
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\RfcComponents_HttpProtocol.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\RfcComponents_WSProtocol.dll" "./"
+
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\libcurl-d.dll" "./"
